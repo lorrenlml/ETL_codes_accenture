839daec4c778fe805ef9179de70d137a642da31e
DG36599 <daniel.gimenezchillada@externos.wizink.es>
2022-02-09 19:35:52 +0100

    Limpieza, fichero parametrico e insert por latest/all

--- 4_validator_ods.py
+++ 4_validator_ods.py
@@ -5,52 +5,17 @@ import sys
 import os
 from datetime import date
 import os
-import subprocess
-client = boto3.client('s3')
-s3 = boto3.resource('s3')
-bucket_obj = s3.Bucket('bucket-wz-aw-dev-euc-external-l0')
-insert_date = str(date.today())
-appName = "Validator_ods"
-master = "local"
+import csv
 
-# Create Spark session
-spark = SparkSession.builder \
-    .appName(appName) \
-    .master(master) \
-    .enableHiveSupport() \
-    .getOrCreate()
+BUCKET_NAME_DEV = 'bucket-wz-aw-dev-euc-external-l0'
+BUCKET_NAME_PROD = 'bucket-wz-aw-prod-euc-external-l0'
+BUCKET_QUERIES_DEV = 'bucket-wz-aw-dev-euc-data-ingestion'
+BUCKET_QUERIES_PROD = 'bucket-wz-aw-prod-euc-data-ingestion'
+PATH_QUERIES = 'validator/queries/'
 
-bucket = 'bucket-wz-aw-dev-euc-data-ingestion'
-queries_path = 'validator/queries/'
-
-
-#Creacion lista/registro 
-df_resultado_n = [] #registro extendido que se genera con cada una de las particiones de las interfaces. Se genera un report para cada interfaz
-
-#TODAS LAS INTERFACES
-interfases_list = ["10002", "10003", "10026", "10029", "10030", "10031", "10308", "20191", "20265", "20275", "20277", 
-"20285", "20386", "20387", "20388", "21059", "21351", "21356", "21710", "21711", "30223", "30307", "30721", "30722", 
-"30723", "30726", "30727", "30728", "30729", "30747", "30751", "30755", "30757", "30758", "30759", "30760", "30761", 
-"30762", "30763", "30765", "30766", "30767", "30768", "30773", "30774", "30775", "30776", "30777", "30778", "30779", 
-"30780", "30781", "30782", "30783", "30784", "30785", "30786", "30789", "30791", "30792", "30798", "30802", "30806", 
-"30817", "30818", "31060", "31061", "31062", "31065", "31094", "31095", "31096", "40049", "40050", "40380", "40382", 
-"40383", "40385", "40386", "40387", "41059", "50351", "50358", "50359", "50360", "50363", "50364", "50365", "50366", 
-"50368", "50369", "50370", "50371", "50372", "50373", "50374", "50377", "50379", "50380", "50381", "50382", "50383", 
-"50384", "50390", "50440", "50603"]
-
-#INTERFACES QUE HAN PASADO EL VALIDATOR EXTERNAL
-passed_gbr_ext = ["10002", "10003", "20386", "20387", "20388", "21059", "21351", "21356", "21710", "21711", "30223", 
-"30721", "30722", "30723", "30726", "30727", "30728", "30765", "30767", "30777", "30779", "30781", "30784", "31096", "40049", "40050", 
-"40380", "40382", "40383", "40385", "40387", "41059", "50358", "50359", "50360", "50363", "50364", "50365", "50440"]
-
-# Tener en cuenta que se ha quitado esta "20191",
-
-schema_prueba = "zerebro_gbr_ods"
-schema_prueba_ext = "zerebro_gbr"
-#spark.sql("drop database {} cascade".format(schema_prueba))
-#SOLO PARA PRUEBAS ESTOS ESQUEMAS
-spark.sql("create database if not exists {}".format(schema_prueba))
-
+# REPORT OUTPUT 
+df_resultado_n = []
+# REPORT SCHEMA
 schema_n = StructType([
     StructField('interface', StringType(), True),
     StructField('partition', StringType(), True),
@@ -62,9 +27,67 @@ schema_n = StructType([
 ])
 
 
+SCHEMA_PRUEBA = "zerebro_gbr_ods"
+SCHEMA_PRUEBA_EXT = "zerebro_gbr"
+
+
+
+# S3 CONFIGURATION
+client = boto3.client('s3')
+s3 = boto3.resource('s3')
+insert_date = str(date.today())
+
+if sys.argv[1][2:] == "prod":
+    bucket_name = BUCKET_NAME_PROD
+    bucket_queries = BUCKET_QUERIES_PROD
+elif sys.argv[1][2:] == "dev":
+    bucket_name = BUCKET_NAME_DEV
+    bucket_queries = BUCKET_QUERIES_DEV
+else:
+    sys.exit("INTRODUCE MODE --dev OR --prod AS SECOND ARGUMENT")
+bucket_obj = s3.Bucket(bucket_name)
+
+
+# CONFIGURATION INTERFACES TO VALIDATE
+if sys.argv[2][2:] == "interfaces.csv":
+    print("INTERFACES TO VALIDATE")
+    filename = sys.argv[2][2:]
+    file = open(filename, "r")
+    reader = csv.reader(file)
+    interfaces = []
+    for line in reader:
+        t=line[0]
+        interfaces.append(t)
+    print(interfaces)
+else:
+    print("VALIDATE ALL INTERFACES WITH QUERIES IN {}/{}".format(bucket_queries, PATH_QUERIES))
+    result = client.list_objects(Bucket=bucket_queries, Prefix=PATH_QUERIES, Delimiter='/')
+    interfaces = []
+    for o in result.get('CommonPrefixes'):
+        interfaces.append(o.get('Prefix').replace(PATH_QUERIES,"")[:-1])
+
+# CONFIGURATION PARTITIONS TO VALIDATE
+if sys.argv[3][2:] == "all":
+    all = "YES"
+else:
+    all = "NO"
+
+# CONFIGURATION SPARK APPLICATION
+appName = "Validator_ods"
+master = "local"
+spark = SparkSession.builder \
+    .appName(appName) \
+    .master(master) \
+    .enableHiveSupport() \
+    .getOrCreate()
+
+
+# VALIDATOR ODS
+spark.sql("create database if not exists {}".format(SCHEMA_PRUEBA))
+
 #Para cada interface, ejecutar las queries create ods e insert
-for interfase in passed_gbr_ext:
-    print(interfase)
+for interface in interfaces:
+    print(interface)
 
     
     #EJECUCION CREATES ODS
@@ -74,23 +97,23 @@ for interfase in passed_gbr_ext:
         
         if i == 1:
             #Ruta archivos queries ods en caso de que sea el primer create que se coge
-            queries_ods = '{queries_path}{interfase}/create_ods_{interfase}.sql'.format(queries_path = queries_path, interfase = interfase)
+            queries_ods = '{queries_path}{interface}/create_ods_{interface}.sql'.format(queries_path = PATH_QUERIES, interface = interface)
 
         else:
             #Ruta archivos queries ods en caso de que no sea el primer create que se coge
-            queries_ods = '{queries_path}{interfase}/create_ods_{interfase}_{n}.sql'.format(queries_path = queries_path, interfase = interfase, n = i)
+            queries_ods = '{queries_path}{interface}/create_ods_{interface}_{n}.sql'.format(queries_path = PATH_QUERIES, interface = interface, n = i)
 
         creates_ods = []
         try: #Por si no existe el CREATE, que no pare de ejecutar
             
             
             #Acceso archivo queries_ods
-            obj = s3.Object(bucket, queries_ods)
+            obj = s3.Object(bucket_queries, queries_ods)
 
             #Lectura y conversion a string
             initial_query = (obj.get()['Body'].read().decode('utf-8'))
 
-            initial_query = initial_query.replace("zerebro_ods", "if not exists " + schema_prueba)
+            initial_query = initial_query.replace("zerebro_ods", "if not exists " + SCHEMA_PRUEBA)
 
             #Dividir las queries por si hay varias en el mismo fichero
             queries = initial_query.split(";")
@@ -109,7 +132,7 @@ for interfase in passed_gbr_ext:
         except Exception as e:
             if i == 1: #Si se esta cogiendo el primer create y falla, habra que generar un reporte de error
                 error = str(e)
-                resultado_n = (interfase, "*","No hay CREATE ODS para esta interface", "KO", "*","*", insert_date)
+                resultado_n = (interface, "*","No hay CREATE ODS para esta interface", "KO", "*","*", insert_date)
                 df_resultado_n.append(resultado_n)
                 break
             else: #Si se esta intentando coger otro create y no hay, no pasa nada
@@ -123,13 +146,13 @@ for interfase in passed_gbr_ext:
                     spark.sql("""{}""".format(creates_ods[k]))
                     print("CREATE ODS {}".format(k + 1) + " hecho")
                     error = "OK"
-                    resultado_n = (interfase, "*","CREATE ODS {}".format(k + 1), "OK", "*","*", insert_date)
+                    resultado_n = (interface, "*","CREATE ODS {}".format(k + 1), "OK", "*","*", insert_date)
                     df_resultado_n.append(resultado_n)
 
 
             except Exception as e:
                 error = str(e)
-                resultado_n = (interfase, "*","CREATE ODS", "KO", error, "*",insert_date)
+                resultado_n = (interface, "*","CREATE ODS", "KO", error, "*",insert_date)
                 df_resultado_n.append(resultado_n)
                 pass
 
@@ -140,7 +163,7 @@ for interfase in passed_gbr_ext:
 
     #Busqueda de country en el nombre de los ficheros
     country = "NOT_VALID"
-    prefix='DM/{}/'.format(interfase)
+    prefix='DM/{}/'.format(interface)
 
     for obj in bucket_obj.objects.filter(Prefix=prefix):
         file = '{0}'.format(obj.key)
@@ -169,31 +192,33 @@ for interfase in passed_gbr_ext:
 
     r = 1
     while r != 20:
-        if country == "NOT_VALID":
+        if (country == "NOT_VALID") or (country != "PT" and country != "ES"):
+            resultado_n = (interface, "*","COUNTRY NO VALIDO", "KO", "*","COUNTRY mal extraido de fichero no esperado", insert_date)
+            df_resultado_n.append(resultado_n)
 
             break
         else:
         
             if r == 1:
                 #Ruta archivos inserts en caso de que sea el primer insert que se coge
-                queries_inserts = '{queries_path}{interfase}/insert_{interfase}.sql'.format(queries_path = queries_path, interfase = interfase)
+                queries_inserts = '{queries_path}{interface}/insert_{interface}.sql'.format(queries_path = PATH_QUERIES, interface = interface)
 
             else:
                 #Ruta archivos inserts en caso de que no sea el primer insert que se coge
-                queries_inserts = '{queries_path}{interfase}/insert_{interfase}_{n}.sql'.format(queries_path = queries_path, interfase = interfase, n = r)
+                queries_inserts = '{queries_path}{interface}/insert_{interface}_{n}.sql'.format(queries_path = PATH_QUERIES, interface = interface, n = r)
 
             inserts = []
             try: #Por si no existe el insert, que no pare de ejecutar
                 
                 
                 #Acceso archivo inserts
-                obj = s3.Object(bucket, queries_inserts)
+                obj = s3.Object(bucket_queries, queries_inserts)
 
                 #Lectura y conversion a string
                 initial_query = (obj.get()['Body'].read().decode('utf-8'))
 
-                initial_query = initial_query.replace("zerebro_ods",schema_prueba)
-                initial_query = initial_query.replace("zerebro_external",schema_prueba_ext)
+                initial_query = initial_query.replace("zerebro_ods",SCHEMA_PRUEBA)
+                initial_query = initial_query.replace("zerebro_external",SCHEMA_PRUEBA_EXT)
 
                 queries = initial_query.split(";")
                 
@@ -208,7 +233,7 @@ for interfase in passed_gbr_ext:
                 if r == 1: #Si se esta cogiendo el primer insert habra que generar un reporte de error
                     error = str(e)
 
-                    resultado_n = (interfase, "*","No hay INSERT para esta interface", "KO", "*","*", insert_date)
+                    resultado_n = (interface, "*","No hay INSERT para esta interface", "KO", "*","*", insert_date)
                     df_resultado_n.append(resultado_n)
                     break
                 else: #Si se esta intentando coger otro insert y no hay, no pasa nada
@@ -221,14 +246,19 @@ for interfase in passed_gbr_ext:
                     #PROCESO DE LANZAR INSERTS
                     for k in range(len(inserts)):
 
-                        new_dates = spark.sql('select (dat_exec_year || dat_exec_month || dat_exec_day) as exec_date from zerebro_gbr.et_{}'.format(interfase)). \
+                        new_dates = spark.sql('select (dat_exec_year || dat_exec_month || dat_exec_day) as exec_date from {}.et_{}'.format(SCHEMA_PRUEBA_EXT, interface)). \
                         distinct().rdd.flatMap(lambda x: x).collect()
+                        # Caso all == "YES"
                         #Se hara el proceso del insert tantas veces como fechas haya en los ficheros de origen, ya que habra que añadir dichas particiones
                         #Definimos una lista con las fechas existentes sin que esten repetidas, cogiendolas de la tabla external
                         #Ordenamos la lista de menor a mayor para coger la fecha mas reciente primero
-                        
-                        new_dates.sort()
-                        print("LA LISTA ORDENADA ES: ", new_dates)
+                        #Caso all == "NO"
+                        if all == "YES":
+                            new_dates.sort()
+                            print("LA LISTA ORDENADA ES: ", new_dates)
+                        else: #all == "NO"
+                            max_date = max(new_dates)
+                            new_dates = [max_date]
                         times = len(new_dates)
                         #Contador latest para controlar la particion snapshot = 'LATEST' y que las demas sean 'HISTORIC'
                         latest = 1
@@ -265,19 +295,18 @@ for interfase in passed_gbr_ext:
                             #Este cast lo hace hive por defecto y por eso recurrimos a lanzar la querie desde aqui
                             os.system('hive -e "{}" -hiveconf hive.execution.engine=tez hive.exec.dynamic.partition=false'.format(insert_prov))
                             
-                            select = spark.sql("SELECT insert_date from {}.ods_{} where exec_date = {} limit 1".format(schema_prueba, interfase, date_part)).take(1)
-                            #print("Este es el output", select)
+                            select = spark.sql("SELECT insert_date from {}.ods_{} where exec_date = {} limit 1".format(SCHEMA_PRUEBA, interface, date_part)).take(1)
                             #En caso de que encontremos datos en la particion en cuestion de la tabla ods
                             if select:
                                 error = 'OK'
                                 print("INSERT {} {}".format(k + 1, date_part) + " EXITOSO")
-                                resultado_n = (interfase, date_part, "INSERT {}".format(k + 1), "OK", "INSERT EJECUTADO", "SELECT DEVUELVE PARTICION", insert_date)
+                                resultado_n = (interface, date_part, "INSERT {}".format(k + 1), "OK", "INSERT EJECUTADO", "SELECT DEVUELVE PARTICION", insert_date)
                                 df_resultado_n.append(resultado_n)
                                 latest += 1
                             else: #En caso de que no encontremos datos en la particion en cuestion de la tabla ods
                                 error = 'KO'
                                 print("INSERT {} {}".format(k + 1, date_part) + " FALLIDO")
-                                resultado_n = (interfase, date_part, "INSERT {}".format(k + 1), "KO", "INSERT EJECUTADO", "SELECT NO DEVUELVE NADA INSERT ERRONEO", insert_date)
+                                resultado_n = (interface, date_part, "INSERT {}".format(k + 1), "KO", "INSERT EJECUTADO", "SELECT NO DEVUELVE NADA INSERT ERRONEO", insert_date)
                                 df_resultado_n.append(resultado_n)
                                 # Convert list to RDD
                                 rdd_n = spark.sparkContext.parallelize(df_resultado_n)
@@ -285,7 +314,7 @@ for interfase in passed_gbr_ext:
                                 # Create data frame
                                 df_n = spark.createDataFrame(rdd_n,schema_n)
 
-                                df_n.coalesce(1).write.option("header", True).mode("overwrite").option("delimiter","|").format("csv").save("s3://bucket-wz-aw-dev-euc-data-ingestion/validator/report_ODS/report_ODS_{}".format(interfase))
+                                df_n.coalesce(1).write.option("header", True).mode("overwrite").option("delimiter","|").format("csv").save("s3://{}/validator/report_ODS/report_ODS_{}_{}_all".format(bucket_queries, interface, insert_date))
                                 break
 
                             # Convert list to RDD
@@ -295,12 +324,12 @@ for interfase in passed_gbr_ext:
                             df_n = spark.createDataFrame(rdd_n,schema_n)
 
 
-                            df_n.coalesce(1).write.option("header", True).mode("overwrite").option("delimiter","|").format("csv").save("s3://bucket-wz-aw-dev-euc-data-ingestion/validator/report_ODS/report_ODS_{}".format(interfase))
+                            df_n.coalesce(1).write.option("header", True).mode("overwrite").option("delimiter","|").format("csv").save("s3://{}/validator/report_ODS/report_ODS_{}_{}_all".format(bucket_queries, interface, insert_date))
 
                 except Exception as e:
                     error = str(e)
                     print(error)
-                    resultado_n = (interfase, date_part,"INSERT", "KO", "INSERT NO EJECUTADO EN HIVE", error, insert_date)
+                    resultado_n = (interface, date_part,"INSERT", "KO", "INSERT NO EJECUTADO EN HIVE", error, insert_date)
                     df_resultado_n.append(resultado_n)
                     
                     pass
